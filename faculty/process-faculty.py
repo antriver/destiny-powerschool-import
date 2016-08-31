cbd8f4a1032c5e5c99eca3534e175171f1b5a2fa
Anthony Kuske <anthonykuske@gmail.com>
2016-08-31 12:58:17 +0100

    Add access level to csv

--- faculty/process-faculty.py
+++ faculty/process-faculty.py
@@ -2,12 +2,24 @@ import csv
 
 newrows = []
 
+def get_access_level(powerschoolID, school):
+	if powerschoolID == 55970 or powerschoolID == 53550:
+		return "Library Assistant"
+	elif powerschoolID == 52950:
+		return "Library Administrator"
+	elif school == 111 or school == 112:
+		return "Teacher"
+	else:
+		return "Staff"
+
 with open('faculty.csv', 'r') as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=',')
 	for row in csvreader:
 
 		powerschoolID = row[0]
 		active = row[4]
+		access_level = get_access_level(powerschoolID, row[5])
+		row.append(access_level)
 
 		if active == '1' and powerschoolID and powerschoolID[:2] != '22' and powerschoolID[:2] != 'xx' and powerschoolID[:6] != 'xbatch' and len(powerschoolID) == 5:
 
