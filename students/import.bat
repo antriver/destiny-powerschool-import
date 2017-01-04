095a4f42c02f6b70844a18533ea18d260309354f
Anthony Kuske <anthonykuske@gmail.com>
2017-01-04 16:00:05 +0000

    Change back after student import

--- students/import.bat
+++ students/import.bat
@@ -9,3 +9,5 @@ cd E:\Follett\FSC-Destiny\fsc\bin\
 PatronImportConverter "E:\DestinyImport\students\students-processed.csv" "E:\DestinyImport\students\students.xml" "E:\DestinyImport\students\convert_students.properties"
 
 updatepatrons "E:\DestinyImport\students\update_students.properties" "E:\DestinyImport\students\students.xml"
+
+cd E:\DestinyImport\students
