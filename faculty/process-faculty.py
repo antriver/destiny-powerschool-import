8ad2c4bd2ef29d6f651c09c70d68e40c69356c02
Anthony Kuske <anthonykuske@gmail.com>
2017-01-04 15:43:29 +0000

    Title added to powerschool export

--- faculty/process-faculty.py
+++ faculty/process-faculty.py
@@ -17,12 +17,12 @@ with open('faculty.csv', 'r') as csvfile:
 	for row in csvreader:
 
 		powerschoolID = row[0]
-		active = row[4]
+		active = row[5]
 		access_level = get_access_level(powerschoolID, row[5])
 		row.append(access_level)
 
 		if active == '1' and powerschoolID and powerschoolID[:2] != '22' and powerschoolID[:2] != 'xx' and powerschoolID[:6] != 'xbatch' and len(powerschoolID) == 5:
-
+			print(row)
 			newrows.append(',' . join(row))
 
 newcsv = open('faculty-processed.csv', 'w')
